7480eda9266ab688ee4c5b86c1f52a12a0d9d1d3
dafotech1 <mayowag80@gmail.com>
2025-11-14 10:27:01 +0000

    Added a valid answer checker when it comes to exiting/continuing the program so the program does not crash.

--- QuizGame.java
+++ QuizGame.java
@@ -40,25 +40,25 @@ public class  QuizGame{
         if (categoryChoice.equals("Physics") || categoryChoice.equals("1")) {
             selectedCategory = physicsQuestions;
             invalidAnswer = false;
-            System.out.println("You have selected " + categoryChoice + " as your category of choice.");
+            System.out.println("You have selected Physics as your category of choice.");
         }
         
         else if (categoryChoice.equals("Maths") || categoryChoice.equals("2")) {
             selectedCategory = mathsQuestions;
             invalidAnswer = false;
-            System.out.println("You have selected " + categoryChoice + " as your category of choice.");
+            System.out.println("You have selected Maths as your category of choice.");
         }
 
         else if (categoryChoice.equals("Geography") || categoryChoice.equals("3")) {
             selectedCategory = geographyQuestions;
             invalidAnswer = false;
-            System.out.println("You have selected " + categoryChoice + " as your category of choice.");
+            System.out.println("You have selected Geography as your category of choice.");
         }
 
         else if (categoryChoice.equals("General Knowledge") || categoryChoice.equals("4")){
             selectedCategory = generalQuestions;
             invalidAnswer = false;
-            System.out.println("You have selected " + categoryChoice + " as your category of choice.");
+            System.out.println("You have selected General Knowledge as your category of choice.");
         }
 
         else {
@@ -86,15 +86,28 @@ public class  QuizGame{
 
         System.out.println("You have reached the end of the quiz. Your final score was " + score + " points.");
         System.out.print("Would you like to select another category or exit the quiz. Please enter 'continue' or 'exit'");
+
+
+        boolean invalidEndChoice = true;
+
+        while (invalidEndChoice) {
+
         String continuationChoice = scanner.nextLine();
 
-        if (continuationChoice.equals("continue")) {
+        if (continuationChoice.equalsIgnoreCase("Continue")) {
             System.out.println("You have selected to continue. Quiz restarting... ");
+            invalidEndChoice = false;
         }
 
-        else if (continuationChoice.equals("exit")) {
+        else if (continuationChoice.equalsIgnoreCase("Exit")) {
             System.out.println("Thanks for playing! Exiting Quiz...");
             continueProgram = false;
+            invalidEndChoice = false;
+        }
+
+        else {
+        System.out.println("Please enter a valid answer");
+        }
         }
     }
 
