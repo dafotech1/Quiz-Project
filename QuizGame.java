8279222c6956640b975164f34737531f5c11bf20
dafotech1 <mayowag80@gmail.com>
2025-12-10 10:32:22 +0000

    indentation

--- QuizGame.java
+++ QuizGame.java
@@ -16,110 +16,111 @@ public class  QuizGame{
 
         // While loop for the main logic to allow the user to either continue the Quiz (select another category) or exit the program when they complete the quiz giving the user the option to play multiple rounds.
         boolean continueProgram = true;
+
         while (continueProgram) {
 
-        System.out.println("""
-                === WELCOME TO DAVID AFOLAYAN'S QUIZ GAME ===
+            System.out.println("""
+                    === WELCOME TO DAVID AFOLAYAN'S QUIZ GAME ===
 
-                Choose one of the following 4 categories!
+                    Choose one of the following 4 categories!
 
-                1. Physics
-                2. Maths
-                3. Geography
-                4. General Knowledge
-                """);
-        
-        //Variables for the user selection and loop processes
-        boolean  invalidAnswer = true;
-        Question selectedCategory[] = null;
-        int score = 0;
-        
-        //This while loop ensures the user inputs a valid answer whereby that be the category number or name, allowing random capitals or spaces in the answer.
-        while (invalidAnswer) {
-
-        //Added scanner.nextLine() as I have found it stops the loop from breaking if they input an invalid answer
-        String categoryChoice = scanner.nextLine();
-        
-        
-        // user selection process, with .trim() to allow any accidental spaces before or after the answer and .equalsIgnoreCase() to also ignore any accidental capitals
-
-        if (categoryChoice.trim().equalsIgnoreCase("Physics") || categoryChoice.equals("1")) {
-            selectedCategory = physicsQuestions;
-            invalidAnswer = false;
-            System.out.println("You have selected Physics as your category of choice.");
-        }
-        
-        else if (categoryChoice.trim().equalsIgnoreCase("Maths") || categoryChoice.equals("2")) {
-            selectedCategory = mathsQuestions;
-            invalidAnswer = false;
-            System.out.println("You have selected Maths as your category of choice.");
-        }
-
-        else if (categoryChoice.trim().equalsIgnoreCase("Geography") || categoryChoice.equals("3")) {
-            selectedCategory = geographyQuestions;
-            invalidAnswer = false;
-            System.out.println("You have selected Geography as your category of choice.");
-        }
-
-        else if (categoryChoice.trim().equalsIgnoreCase("General Knowledge") || categoryChoice.equals("4")){
-            selectedCategory = generalQuestions;
-            invalidAnswer = false;
-            System.out.println("You have selected General Knowledge as your category of choice.");
-        }
-
-        else {
-            System.out.println("Invalid answer... please enter either the category name or number. ");
-        }
-
-    }
-
-        System.out.println("=== The Quiz is now starting!!! ===");
-
-        // used iteration alongside with scanner.nextLine to print each question in the array individually allowing the user to enter their answer one at a time, seeing if their answer is right or wrong straightaway
-
-        for (Question q : selectedCategory) {
-            System.out.println(q.getQuestionText());
-            String userAnswer = scanner.nextLine();
-
-            if (q.checkAnswer(userAnswer)) {
-                System.out.println(userAnswer + " is the correct answer!");
-                score++; //adds one to the score every time the user inputs the correct answer.
-                System.out.println("Your score now is " + score + " points.");
-            }
+                    1. Physics
+                    2. Maths
+                    3. Geography
+                    4. General Knowledge
+                    """);
             
-            else {
-                System.out.println("Your answer is incorrect :( The correct answer was " + q.getAnswer());
-                System.out.println("Your score remains " + score + " points.");
+            //Variables for the user selection and loop processes
+            boolean  invalidAnswer = true;
+            Question selectedCategory[] = null;
+            int score = 0;
+            
+            //This while loop ensures the user inputs a valid answer whereby that be the category number or name, allowing random capitals or spaces in the answer.
+            while (invalidAnswer) {
+
+                //Added scanner.nextLine() as I have found it stops the loop from breaking if they input an invalid answer
+                String categoryChoice = scanner.nextLine();
+                
+                
+                // user selection process, with .trim() to allow any accidental spaces before or after the answer and .equalsIgnoreCase() to also ignore any accidental capitals
+
+                if (categoryChoice.trim().equalsIgnoreCase("Physics") || categoryChoice.equals("1")) {
+                    selectedCategory = physicsQuestions;
+                    invalidAnswer = false;
+                    System.out.println("You have selected Physics as your category of choice.");
+                }
+                
+                else if (categoryChoice.trim().equalsIgnoreCase("Maths") || categoryChoice.equals("2")) {
+                    selectedCategory = mathsQuestions;
+                    invalidAnswer = false;
+                    System.out.println("You have selected Maths as your category of choice.");
+                }
+
+                else if (categoryChoice.trim().equalsIgnoreCase("Geography") || categoryChoice.equals("3")) {
+                    selectedCategory = geographyQuestions;
+                    invalidAnswer = false;
+                    System.out.println("You have selected Geography as your category of choice.");
+                }
+
+                else if (categoryChoice.trim().equalsIgnoreCase("General Knowledge") || categoryChoice.equals("4")){
+                    selectedCategory = generalQuestions;
+                    invalidAnswer = false;
+                    System.out.println("You have selected General Knowledge as your category of choice.");
+                }
+
+                else {
+                    System.out.println("Invalid answer... please enter either the category name or number. ");
+                }
+
             }
-        }
 
-        System.out.println("You have reached the end of the quiz. Your final score was " + score + " points.");
-        System.out.println("Would you like to select another category or exit the quiz? " +
-                            "Please enter 'continue' or 'exit'");
+            System.out.println("=== The Quiz is now starting!!! ===");
 
-        // assigned a boolean variable (for a loop in case the user inputs an invalid answer) 
-        // selection to control the user input when deciding whether to continue or exit the program.
-        boolean invalidEndChoice = true;
+            // used iteration alongside with scanner.nextLine to print each question in the array individually allowing the user to enter their answer one at a time, seeing if their answer is right or wrong straightaway
 
-        while (invalidEndChoice) {
+            for (Question q : selectedCategory) {
+                System.out.println(q.getQuestionText());
+                String userAnswer = scanner.nextLine();
 
-            String continuationChoice = scanner.nextLine();
-
-            if (continuationChoice.equalsIgnoreCase("Continue")) {
-                System.out.println("You have selected to continue. Quiz restarting... ");
-                invalidEndChoice = false;
+                if (q.checkAnswer(userAnswer)) {
+                    System.out.println(userAnswer + " is the correct answer!");
+                    score++; //adds one to the score every time the user inputs the correct answer.
+                    System.out.println("Your score now is " + score + " points.");
+                }
+                
+                else {
+                    System.out.println("Your answer is incorrect :( The correct answer was " + q.getAnswer());
+                    System.out.println("Your score remains " + score + " points.");
+                }
             }
 
-            else if (continuationChoice.equalsIgnoreCase("Exit")) {
-                System.out.println("Thanks for playing! Exiting Quiz...");
-                continueProgram = false;
-                invalidEndChoice = false;
-            }
+            System.out.println("You have reached the end of the quiz. Your final score was " + score + " points.");
+            System.out.println("Would you like to select another category or exit the quiz? " +
+                                "Please enter 'continue' or 'exit'");
 
-            else {
-            System.out.println("Please enter a valid answer");
+            // assigned a boolean variable (for a loop in case the user inputs an invalid answer) 
+            // selection to control the user input when deciding whether to continue or exit the program.
+            boolean invalidEndChoice = true;
+
+            while (invalidEndChoice) {
+
+                String continuationChoice = scanner.nextLine();
+
+                if (continuationChoice.equalsIgnoreCase("Continue")) {
+                    System.out.println("You have selected to continue. Quiz restarting... ");
+                    invalidEndChoice = false;
+                }
+
+                else if (continuationChoice.equalsIgnoreCase("Exit")) {
+                    System.out.println("Thanks for playing! Exiting Quiz...");
+                    continueProgram = false;
+                    invalidEndChoice = false;
+                }
+
+                else {
+                System.out.println("Please enter a valid answer");
+                }
             }
-        }
     }
 
     }
--- QuizTest.java
+++ QuizTest.java
@@ -5,98 +5,98 @@ public class QuizTest {
     public static void main(String[] args) {
 
     System.out.println("Test 1: Testing my question constructor");
-    Question question = new Question("Is this constructor valid", "Yes", "Test"); //created a Question for the Question and Answer to be tested whether or not they store the intended value/string
+        Question question = new Question("Is this constructor valid", "Yes", "Test"); //created a Question for the Question and Answer to be tested whether or not they store the intended value/string
 
-    if (question.getQuestionText().equals("Is this constructor valid")) {
-        System.out.println("PASS");
-    }else{
-        System.out.println("FAIL");
-    }
+        if (question.getQuestionText().equals("Is this constructor valid")) {
+            System.out.println("PASS");
+        }else{
+            System.out.println("FAIL");
+        }
 
-    if (question.getAnswer().equals("Yes")) {
-        System.out.println("PASS");
-    }else{
-        System.out.println("FAIL");
-    }
+        if (question.getAnswer().equals("Yes")) {
+            System.out.println("PASS");
+        }else{
+            System.out.println("FAIL");
+        }
 
 
     System.out.println("Test 2: Testing my checkAnswer()"); //testing whether my checkAnswer() works as intended... does it allow spaces, does it allow random capitals and simply does it allow incorrect or the exact correct answer?
-    Question question2 = new Question("Does checkAnswer() work with the set answer of 'Photon', with all variations of the answer ", "Photon", "Test");
+        Question question2 = new Question("Does checkAnswer() work with the set answer of 'Photon', with all variations of the answer ", "Photon", "Test");
 
-    if (question2.checkAnswer("Photon")) {
-        System.out.println("PASS");
-    }else {
-        System.out.println("FAIL");
-    }
+        if (question2.checkAnswer("Photon")) {
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
 
-    if (question2.checkAnswer("photon")) {
-        System.out.println("PASS");
-    }else {
-        System.out.println("FAIL");
-    }
+        if (question2.checkAnswer("photon")) {
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
 
-    if (question2.checkAnswer("PHOTON   ")) {
-        System.out.println("PASS");
-    }else {
-        System.out.println("FAIL");
-    }
+        if (question2.checkAnswer("PHOTON   ")) {
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
 
-    if (question2.checkAnswer("   PhOTON   ")) {
-        System.out.println("PASS");
-    }else {
-        System.out.println("FAIL");
-    }
-    if (question2.checkAnswer("Electron") == false ){
-        System.out.println("PASS");
-    }else {
-        System.out.println("FAIL");
-    }
+        if (question2.checkAnswer("   PhOTON   ")) {
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
+        if (question2.checkAnswer("Electron") == false ){
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
 
     System.out.println("Test 3: Testing QuestionBank");
 
     //Testing the existing arrays in QuestionBank to check whether the Questions are storing the correct amount of questions that I have created to be used.
-    Question[] physicsQuestions = QuestionBank.getPhysicsQuestions();
-    Question[] mathsQuestions = QuestionBank.getMathsQuestions();
-    Question[] geographyQuestions = QuestionBank.getGeographyQuestions();
-    Question[] generalQuestions = QuestionBank.getGeneralQuestions();
+        Question[] physicsQuestions = QuestionBank.getPhysicsQuestions();
+        Question[] mathsQuestions = QuestionBank.getMathsQuestions();
+        Question[] geographyQuestions = QuestionBank.getGeographyQuestions();
+        Question[] generalQuestions = QuestionBank.getGeneralQuestions();
 
-    if (physicsQuestions.length == 5) {
-        System.out.println("PASS");
-    }else {
-        System.out.println("FAIL");
-    }
+        if (physicsQuestions.length == 5) {
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
 
-    if (mathsQuestions.length == 5) {
-        System.out.println("PASS");
-    }else {
-        System.out.println("FAIL");
-    }
+        if (mathsQuestions.length == 5) {
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
 
-    if (geographyQuestions.length == 6) {
-        System.out.println("PASS");
-    }else {
-        System.out.println("FAIL");
-    }
+        if (geographyQuestions.length == 6) {
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
 
-    if (generalQuestions.length == 5) {
-        System.out.println("PASS");
-    }else {
-        System.out.println("FAIL");
-    }
+        if (generalQuestions.length == 5) {
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
 
     System.out.println("Test 4: Testing score logic");
-    int score = 0;
-    Question q = new Question("What is 2+2?","4","Maths");
+        int score = 0;
+        Question q = new Question("What is 2+2?","4","Maths");
 
-    if (q.checkAnswer("4")) {
-        score++;
-    }
+        if (q.checkAnswer("4")) {
+            score++;
+        }
 
-    if (score == 1) {
-        System.out.println("PASS");
-    }else{
-        System.out.println("FAIL");
-    }
+        if (score == 1) {
+            System.out.println("PASS");
+        }else{
+            System.out.println("FAIL");
+        }
     
     }
 }
